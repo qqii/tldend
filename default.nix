a4fb785629fdd8a811225af960a015b22a9e005e
qqii <Qi-rui.Chen@hotmail.co.uk>
2019-07-08 09:47:33 +0100

    Add tldin that is less strict about end of line

--- default.nix
+++ default.nix
@@ -5,8 +5,11 @@ let
     tldend = pkgs.writeShellScriptBin "tldend" ''
         grep --color -E $(${pkgs.curl}/bin/curl --silent https://publicsuffix.org/list/public_suffix_list.dat | sed -E '/\/\/|^$|\./d' | tr '\n' '|' | sed 's/|/$&/g;s/.$//')
     '';
+    tldin = pkgs.writeShellScriptBin "tldin" ''
+        grep --color -E $(${pkgs.curl}/bin/curl --silent https://publicsuffix.org/list/public_suffix_list.dat | sed -E '/\/\/|^$|\./d' | tr '\n' '|' | sed 's/|/\\W&/g;s/.$//')
+    '';
 in stdenv.mkDerivation rec {
     name = "tldend";
 
-    buildInputs = [ tldend ];
+    buildInputs = [ tldend tldin ];
 }
